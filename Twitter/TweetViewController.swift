f5cee118668610b025ea4f6ca61c5fd14a68304b
Lily Pham <lilypham@lilys-macbook-pro.local>
2019-02-10 01:52:01 -0800

    API call to post tweet

--- Twitter/TweetViewController.swift
+++ Twitter/TweetViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class TweetViewController: UIViewController {
 
+    @IBOutlet weak var tweetTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +23,16 @@ class TweetViewController: UIViewController {
     }
     
     @IBAction func tweet(_ sender: Any) {
+        if (!tweetTextView.text.isEmpty) {
+            TwitterAPICaller.client?.postTweet(tweetString: tweetTextView.text, success: {
+                self.dismiss(animated: true, completion: nil)
+            }, failure: { (error) in
+                print("Error posting tweet \(error)")
+                self.dismiss(animated: true, completion: nil)
+            })
+        } else {
+            self.dismiss(animated: true, completion: nil)
+        }
     }
     
     /*
