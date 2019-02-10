b8f0d169bc992a7a9a2087d3718747555ba125a1
Lily Pham <lilypham@lilys-macbook-pro.local>
2019-02-10 02:01:46 -0800

    Keyboard and cursor automatically appears on the tweet view

--- Twitter/TweetViewController.swift
+++ Twitter/TweetViewController.swift
@@ -14,8 +14,7 @@ class TweetViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tweetTextView.becomeFirstResponder()
     }
     
     @IBAction func cancel(_ sender: Any) {
